591c10f31c7d984adbc9448bc0a0644a03fbff42
Corendos <corentin.godeau37@gmail.com>
2019-08-16 23:03:25 +0200

    wip: testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
+#include <chrono>
 
-#include <NFA.hpp>
+#include "NFA.hpp"
 
 int main() {
-    Alphabet alphabet{"abc"};
-    NFA<TokenInfo> nfa{alphabet};
+    Alphabet alphabet{"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_.+-"};
+    NFA nfa{alphabet};
 
-    std::vector<TokenInfo> tokenInfo1 = {TokenInfo("identifier", 10)};
-    std::vector<TokenInfo> tokenInfo2 = {TokenInfo("number", 10)};
-
-    State<std::vector<TokenInfo>> s1("S1", std::vector<TokenInfo>(), false, true);
-    State<std::vector<TokenInfo>> s2("S2");
-    State<std::vector<TokenInfo>> s3("S3");
-    State<std::vector<TokenInfo>> s4("S4", tokenInfo1, true);
-    State<std::vector<TokenInfo>> s5("S5");
-    State<std::vector<TokenInfo>> s6("S6");
-    State<std::vector<TokenInfo>> s7("S7", tokenInfo2, true);
-    State<std::vector<TokenInfo>> s8("S8");
+    State<StatePayload> s1{"S1", StatePayload(), false, true};
+    State<StatePayload> s2{"S2"};
+    State<StatePayload> s3{"S3"};
+    State<StatePayload> s4{"S4", StatePayload{{{"IF", 10}}}, true};
+    State<StatePayload> s5{"S5"};
+    State<StatePayload> s6{"S6", StatePayload{{{"ID", 10}}}, true};
+    State<StatePayload> s7{"S7"};
+    State<StatePayload> s8{"S8"};
+    State<StatePayload> s9{"S9", StatePayload{{{"NUM", 10}}}, true};
+    State<StatePayload> s10{"S10"};
+    State<StatePayload> s11{"S11"};
+    State<StatePayload> s12{"S12", StatePayload{{{"FLOAT", 10}}}, true};
+    State<StatePayload> s13{"S13", StatePayload{{{"FLOAT", 10}}}, true};
+    State<StatePayload> s14{"S14"};
+    State<StatePayload> s15{"S15", StatePayload{{{"FLOAT", 10}}}, true};
+    State<StatePayload> s16{"S16"};
+    State<StatePayload> s17{"S17"};
+    State<StatePayload> s18{"S18", StatePayload{{{"FLOAT", 10}}}, true};
 
     nfa.addState(s1);
     nfa.addState(s2);
@@ -26,18 +34,59 @@ int main() {
     nfa.addState(s6);
     nfa.addState(s7);
     nfa.addState(s8);
+    nfa.addState(s9);
+    nfa.addState(s10);
+    nfa.addState(s11);
+    nfa.addState(s12);
+    nfa.addState(s13);
+    nfa.addState(s14);
+    nfa.addState(s15);
+    nfa.addState(s16);
+    nfa.addState(s17);
+    nfa.addState(s18);
 
     nfa.addTransition(s1, s2);
-    nfa.addTransition(s1, s5);
-    nfa.addTransition(s2, 'a', s3);
-    nfa.addTransition(s3, 'c', s4);
-    nfa.addTransition(s5, s6);
-    nfa.addTransition(s5, s7);
-    nfa.addTransition(s6, 'a', s8);
-    nfa.addTransition(s7, 'b', s8);
-    nfa.addTransition(s8, s1);
+    nfa.addTransition(s2, 'i', s3);
+    nfa.addTransition(s3, 'f', s4);
 
-    NFA<TokenInfo> dfa = nfa.toDFA();
+    nfa.addTransition(s1, s5);
+    nfa.addTransitions(s5, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_", s6);
+    nfa.addTransitions(s6, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789", s6);
+
+    nfa.addTransition(s1, s7);
+    nfa.addTransitions(s7, "+-", s8);
+    nfa.addTransition(s7, s8);
+    nfa.addTransitions(s8, "0123456789", s9);
+    nfa.addTransition(s9, s8);
+
+    nfa.addTransition(s1, s10);
+    nfa.addTransitions(s10, "+-", s11);
+    nfa.addTransition(s10, s11);
+    nfa.addTransitions(s11, "0123456789", s12);
+    nfa.addTransitions(s12, "0123456789", s12);
+    nfa.addTransition(s12, '.', s13);
+    nfa.addTransitions(s13, "0123456789", s13);
+    nfa.addTransition(s11, '.', s14);
+    nfa.addTransition(s15, s14);
+    nfa.addTransitions(s14, "0123456789", s15);
+    nfa.addTransitions(s15, "eE", s16);
+    nfa.addTransitions(s12, "eE", s16);
+    nfa.addTransitions(s13, "eE", s16);
+    nfa.addTransitions(s16, "+-", s17);
+    nfa.addTransition(s16, s17);
+    nfa.addTransitions(s17, "0123456789", s18);
+    nfa.addTransition(s18, s17);
+
+    //nfa.printDebug();
+
+    auto start = std::chrono::high_resolution_clock::now();
+    NFA dfa = nfa.toDFA();
     dfa.printDebug();
+    auto end = std::chrono::high_resolution_clock::now();
+
+    uint64_t nanoseconds = std::chrono::duration<uint64_t, std::nano>(end - start).count();
+    uint64_t microseconds = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    std::cout << nanoseconds << " ns" << std::endl;
+    std::cout << microseconds << " µs" << std::endl;
     return 0;
 }
